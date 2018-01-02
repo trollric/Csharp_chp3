913de80fc4e4b2f702f5e0edb4ae1d11f1ef629b
Marcus Hansson <90m.hansson@gmail.com>
2018-01-02 11:56:54 +0100

    First example done

---
+++ SimpleClassExample/SimpleClassExample/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        // The "state" of the Car.
+        public int currentSpeed;
+        public string petName;
+
+        // The functionality of the Car.
+        public void speedUp(int delta)
+        {
+            currentSpeed += delta;
+        }
+
+        public void PrintState()
+        {
+            Console.WriteLine("\n{0} is going {1}KM/H.",petName,currentSpeed);
+        }
+    }
+}
--- SimpleClassExample/SimpleClassExample/Program.cs
+++ SimpleClassExample/SimpleClassExample/Program.cs
@@ -10,6 +10,19 @@ namespace SimpleClassExample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** Fun with Class Types ******\n");
+            // Allocate and configure a Car object.
+            Car myCar = new Car();
+            myCar.petName = "Henry";
+            myCar.currentSpeed = 10;
+            // Speed up the car a few times and print out the
+            // new state.
+            for (int i = 0; i < 10; i++)
+            {
+                myCar.speedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
         }
     }
 }
