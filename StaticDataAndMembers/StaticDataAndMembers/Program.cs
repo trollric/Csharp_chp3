1d2d44a441c24e1a7d89a89069a53bff18a1a56f
Marcus Hansson <90m.hansson@gmail.com>
2018-01-04 15:02:56 +0100

    Finished intro to statics

--- StaticDataAndMembers/StaticDataAndMembers/Program.cs
+++ StaticDataAndMembers/StaticDataAndMembers/Program.cs
@@ -10,7 +10,18 @@ namespace StaticDataAndMembers
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("****** Fun with Static Data ******\n");
+            SavingsAccount s1 = new SavingsAccount(50);
+            SavingsAccount s2 = new SavingsAccount(100);
+            // Print current interest rate
+            Console.WriteLine("The current intrest is {0}", SavingsAccount.GetIntrestRate());
+            // Change the intrest rate
+            SavingsAccount.SetIntrestRate(0.05);
+            SavingsAccount s3 = new SavingsAccount(10000.75);
+            // After we make a new instance of SavingsAccount the static is not "reset"
+            Console.WriteLine("The current intrest is {0}", SavingsAccount.GetIntrestRate());
+            
+            Console.ReadLine();
         }
     }
 }
---
+++ StaticDataAndMembers/StaticDataAndMembers/SavingsAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticDataAndMembers
+{
+    class SavingsAccount
+    {
+        // Instance-level data.
+        public double currBalance;
+        public static double currInterestRate = 0.04;
+
+        public SavingsAccount(double balance)
+        {
+            currBalance = balance;
+        }
+
+        static SavingsAccount()
+        {
+            Console.WriteLine("In static CTOR");
+            currInterestRate = 0.04;
+        }
+
+        public static void SetIntrestRate(double newIntrest)
+        {
+            currInterestRate = newIntrest;
+        }
+
+        public static double GetIntrestRate()
+        {
+            return currInterestRate;
+        }
+    }
+}
