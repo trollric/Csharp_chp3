d7e43d798e06b1ee0817517009d4aaac99d54e2d
Marcus Hansson <90m.hansson@gmail.com>
2018-01-12 11:38:32 +0100

    Overriding System.Object methods

---
+++ ObjectOverrides/ObjectOverrides/Person.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    // Rememver that Person extends object
+    class Person
+    {
+        public string Name { get; set; } = "";
+        public string Surname { get; set; } = "";
+        public int Age { get; set; }
+        public string SSN { get; set; } = "1992-11-23-4586";
+
+        public Person()
+        {
+
+        }
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            string myState;
+            myState = string.Format("[Name: {0}; Surname: {1}; Age: {2}]", Name, Surname, Age);
+            return myState;
+        }
+
+        public override bool Equals(object obj)
+        {
+            //if(obj is Person && obj != null)
+            //{
+            //    Person temp = (Person)obj;
+            //    if(temp.Name == this.Name && temp.Surname == this.Surname && temp.Age == this.Age)
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+            //return false;
+            return obj.ToString() == this.ToString();
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+    }
+}
---
+++ ObjectOverrides/ObjectOverrides/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with System.Object ******\n");
+            Person p1 = new Person();
+            Console.WriteLine("ToString: {0}",p1.ToString());
+            Console.WriteLine("Hash code: {0}",p1.GetHashCode());
+            Console.WriteLine("Type: {0}", p1.GetType());
+            // Make some other references to p1.
+            Person p2 = p1;
+            object o = p2;
+            // Are the references poiting to the same object in memory?
+            if(o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Same instance!");
+            }
+            Console.WriteLine();
+            TestModifiedClass();
+            Console.ReadLine();
+        }
+
+        private static void TestModifiedClass()
+        {
+            // NOTE: we want these to be identical to test
+            // the Equals() and GetHashCode() methods.
+            Person p1 = new Person("Homer", "Simpson", 50);
+            Person p2 = new Person("Homer", "Simpson", 50);
+            // Get stringified version of objects.
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}",p2.ToString());
+            Console.WriteLine();
+            // Test overriden Equals().
+            Console.WriteLine("p1 = p2?: {0}",p1.Equals(p2));
+            // test hashcodes
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+            Console.WriteLine();
+            // Change age of p2 and repeat the tests
+            p2.Age = 45;
+            Console.WriteLine("p1.ToString() = {0}", p1.ToString());
+            Console.WriteLine("p2.ToString() = {0}", p2.ToString());
+            Console.WriteLine();
+            // Test overriden Equals().
+            Console.WriteLine("p1 = p2?: {0}", p1.Equals(p2));
+            // test hashcodes
+            Console.WriteLine("Same hash codes?: {0}", p1.GetHashCode() == p2.GetHashCode());
+        }
+    }
+}
