4dc8f7c7261baabab46af7ae2c574bb42427e1b7
Marcus Hansson <90m.hansson@gmail.com>
2018-01-18 15:55:49 +0100

    Catching multiple exceptions

---
+++ ProcessMultipleExceptions/ProcessMultipleExceptions/CarIsDeadException.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExceptions
+{
+    [Serializable]
+    class CarIsDeadException : ApplicationException
+    {
+        private string messageDetails = String.Empty;
+        public DateTime ErrorTimeStamp { get; set; }
+        public string CauseOfError { get; set; }
+
+        public CarIsDeadException()
+        {
+
+        }
+        public CarIsDeadException(string message) : base(message)
+        {
+
+        }
+        public CarIsDeadException(string message, System.Exception inner) : base(message,inner)
+        {
+
+        }
+        protected CarIsDeadException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context)
+            : base(info, context) { }
+
+        public CarIsDeadException(string message, string cause, DateTime time) : base(message)
+        {
+            CauseOfError = cause;
+            ErrorTimeStamp = time;
+        }
+        // Override the TestSnippetException.Message property
+        public override string Message
+        {
+            get
+            {
+                return string.Format("Car Error Message: {0}", messageDetails);
+            }
+        }
+    }
+}
---
+++ ProcessMultipleExceptions/ProcessMultipleExceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcessMultipleExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Handlong Multiple Exceptions ******\n");
+            Car myCar = new Car("Rusty", 90);
+            try
+            {
+                // Trip arg out of range exception.
+                myCar.Accelerate(-10);
+            }
+            catch(CarIsDeadException e) when (e.ErrorTimeStamp.DayOfWeek != DayOfWeek.Friday)
+            {
+                // This line will only print if the "when" keyword evaluates true
+                Console.WriteLine(e.Message);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            // Safeguard catch
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            // This piece of code will always occur Exception or not
+            finally
+            {
+                myCar.CrankTunes(false);
+            }
+            Console.ReadLine();
+        }
+    }
+}
