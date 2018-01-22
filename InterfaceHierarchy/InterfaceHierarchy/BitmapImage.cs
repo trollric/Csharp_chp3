28cb847c8132a977b037076b67ea7f2835e52880
Marcus Hansson <90m.hansson@gmail.com>
2018-01-22 12:56:09 +0100

    Simple Interface Hierachy tested.

---
+++ InterfaceHierarchy/InterfaceHierarchy/BitmapImage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceHierarchy
+{
+    public class BitmapImage : IAdvancedDraw
+    {
+        public void Draw()
+        {
+            Console.WriteLine("Drawing...");
+        }
+
+        public void DrawInBoundingBox(int top, int left, int right, int bottom)
+        {
+            Console.WriteLine("Drawing in a box...");
+        }
+
+        public void DrawUpsideDown()
+        {
+            Console.WriteLine("Drawing upside down!");
+        }
+    }
+}
