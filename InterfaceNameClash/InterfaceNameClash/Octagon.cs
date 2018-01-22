c8b69c059ab23115ee00c7eccafca9222b2e2908
Marcus Hansson <90m.hansson@gmail.com>
2018-01-22 11:25:38 +0100

    Testing out some name clashing

---
+++ InterfaceNameClash/InterfaceNameClash/Octagon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Octagon : IDrawToForm, IDrawToMemory, IDrawToPrinter
+    {
+        // Shared Draw():ing logic
+        void IDrawToForm.Draw()
+        {
+            Console.WriteLine("Drawing to the Form...");
+        }
+        void IDrawToMemory.Draw()
+        {
+            Console.WriteLine("Drawing to the Memory...");
+        }
+        void IDrawToPrinter.Draw()
+        {
+            Console.WriteLine("Drawing to the Printer...");
+        }
+    }
+}
---
+++ InterfaceNameClash/InterfaceNameClash/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceNameClash
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with Interface Name Clashes ******\n");
+            // All of these invocations call
+            // the same Draw() method.
+            Octagon oct = new Octagon();
+            IDrawToForm itfForm = (IDrawToForm)oct;
+            itfForm.Draw();
+            IDrawToPrinter itfPrinter = (IDrawToPrinter)oct;
+            itfPrinter.Draw();
+            IDrawToMemory itfMemory = (IDrawToMemory)oct;
+            itfMemory.Draw();
+            // We now must use casting to acces the Draw() members.
+            Console.WriteLine();
+            ((IDrawToPrinter)oct).Draw();
+            // Could also use the is keyword
+            if(oct is IDrawToMemory)
+            {
+                ((IDrawToMemory)oct).Draw();
+            }
+            Console.ReadLine();
+        }
+    }
+}
