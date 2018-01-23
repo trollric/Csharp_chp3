52fdd9dd6b9658aa2b1e3a863322616abba4bf18
Marcus Hansson <90m.hansson@gmail.com>
2018-01-23 11:02:32 +0100

    Learning about ICloneable

---
+++ CloneablePoint/CloneablePoint/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        private PointDescription desc = new PointDescription();
+        public PointDescription Desc
+        {
+            get { return desc; }
+            set { desc = value; }
+        }
+
+        public Point()
+        {
+
+        }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos; Y = yPos;
+        }
+        public Point(int xPos, int yPos, string petName)
+        {
+            X = xPos; Y = yPos;
+            desc.PetName = petName;
+        }
+
+        // Override Object.ToString().
+        public override string ToString()
+        {
+            return string.Format("X = {0}; Y = {1}; Name = {2};\nID = {3}\n", X, Y,desc.PetName,desc.PointID);
+        }
+        // You can simplify since Point doesn't hold any ref types
+        public object Clone()
+        {
+            // First get a shallow copy
+            Point newPoint = (Point)this.MemberwiseClone();
+            // Then fill in the gaps
+            PointDescription currentDesc = new PointDescription();
+            currentDesc.PetName = this.desc.PetName;
+            newPoint.desc = currentDesc;
+            return newPoint;
+        }
+    }
+}
---
+++ CloneablePoint/CloneablePoint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("****** Fun with Object Cloning ******\n");
+            // Two references to the same object.
+            Point p1 = new Point(50, 50);
+            Point p2 = (Point)p1.Clone();
+            p2.X = 0;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+            Console.WriteLine();
+            Console.WriteLine("Created and cloned p3 to a new Point p4");
+            Point p3 = new Point(100, 100, "Jane");
+            Point p4 = (Point)p3.Clone();
+            Console.WriteLine("Before modification:");
+            Console.WriteLine("p3: {0}", p3);
+            Console.WriteLine("p4: {0}",p4);
+            p4.Desc.PetName = "My generic clone";
+            p4.X = 9;
+            Console.WriteLine("\nChanged p.4desc.petName and p4.X");
+            Console.WriteLine("After modification");
+            Console.WriteLine("p3: {0}",p3);
+            Console.WriteLine("p4: {0}",p4);
+            Console.ReadLine();
+        }
+    }
+}
