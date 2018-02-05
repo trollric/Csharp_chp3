a32b5847aa74e9978903fd75b1d6b7852d4b348a
Marcus Hansson <90m.hansson@gmail.com>
2018-02-05 13:23:25 +0100

    Put my custom generics into a seperate class.

---
+++ CustomGenericMethods/CustomGenericMethods/MyGenericMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericMethods
+{
+    public static class MyGenericMethods
+    {
+
+        public static void DisplayBaseClass<T>()
+        {
+            Console.WriteLine("Base class of {0} is: {1}", typeof(T), typeof(T).BaseType);
+        }
+
+        // This method will swap any two items.
+        // as specified by the type parameter <T>
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine("You sent the Swap() method a {0}", typeof(T));
+            T temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
--- CustomGenericMethods/CustomGenericMethods/Program.cs
+++ CustomGenericMethods/CustomGenericMethods/Program.cs
@@ -14,30 +14,25 @@ namespace CustomGenericMethods
             // Swap 2 ints
             int a = 10, b = 90;
             Console.WriteLine("Before the swap a = {0}, b = {1}",a,b);
-            Swap(ref a, ref b);
+            MyGenericMethods.Swap(ref a, ref b);
             Console.WriteLine("After the swap a = {0}, b = {1}",a,b);
             Console.WriteLine();
             // Swap two strings
             string s1 = "Hello", s2 = "World";
             Console.WriteLine("Before swap it reads: {0} {1}", s1, s2);
-            Swap(ref s1, ref s2);
+            MyGenericMethods.Swap(ref s1, ref s2);
             Console.WriteLine("After swap it reads: {0} {1}", s1, s2);
             // Swap two bool
             bool b1 = true, b2 = false;
             Console.WriteLine("Before swap it reads: {0} {1}", b1, b2);
-            Swap<bool>(ref b1, ref b2);
+            MyGenericMethods.Swap<bool>(ref b1, ref b2);
             Console.WriteLine("After swap it reads: {0} {1}", b1, b2);
+            MyGenericMethods.DisplayBaseClass<int>();
+            MyGenericMethods.DisplayBaseClass<string>();
+            // Compiler error must supply base class
+            // DisplayBaseClass();
             Console.ReadLine();
         }
 
-        // This method will swap any two items.
-        // as specified by the type parameter <T>
-        private static void Swap<T>(ref T a, ref T b)
-        {
-            Console.WriteLine("You sent the Swap() method a {0}",typeof(T));
-            T temp = a;
-            a = b;
-            b = temp;
-        }
     }
 }
