1ac0ee5b86baf588796cac3c88cd9e5229baf860
Marcus Hansson <90m.hansson@gmail.com>
2018-09-03 21:03:10 +0200

    finish point generic

--- GenericPoint/GenericPoint/Program.cs
+++ GenericPoint/GenericPoint/Program.cs
@@ -20,7 +20,7 @@ namespace GenericPoint
         public T Y
         {
             get { return yPos; }
-            set { yPos = value}
+            set { yPos = value; }
         }
         // Generic constructor.
         public Point(T xVal, T yVal)
@@ -44,6 +44,20 @@ namespace GenericPoint
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("***** Fun with Generic Structures *****\n");
+            // Point using ints
+            Point<int> p = new Point<int>(10, 10);
+            Console.WriteLine("p.toString() = {0}", p.ToString());
+            p.ResetPoint();
+            Console.WriteLine("p.toString() = {0}",p.ToString());
+            Console.WriteLine();
+            // Points using doubles
+            Point<double> p2= new Point<double>(5.4, 3.2);
+            Console.WriteLine("p2.toString() = {0}", p2.ToString());
+            p2.ResetPoint();
+            Console.WriteLine("p2.toString() = {0}", p2.ToString());
+            Console.ReadLine();
+
         }
     }
 }
